b9d9560623f82aefac28ca8c4012a654873be844
brc0d3s <brc0d3s@gmail.com>
2023-12-31 20:01:38 +0300

    loginpage

---
+++ DevHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="DevHub.Global" %>
---
+++ DevHub/default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace DevHub
+{
+
+    public partial class LoginForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void LoginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
